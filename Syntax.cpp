b1d18c5b33816e0f8d98c647eeb63abedb92f933
Sreynaj Keb <kebsreynaj2@gmail.com>
2022-08-16 22:58:57 +0700

    Source Code

---
+++ Syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+
+//print the output
+int main(){
+    cout <<"Hello! I am SREYNAJ" ;
+    cout <<"\nI am learning how to program in C++" << endl;
+    cout <<"I think it really is nth much diff from C" ;
+    return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//Types of variables
+
+int main(){
+    //integer
+    int myNum = 17;
+    cout << myNum << endl; 
+
+    //float numbers
+    float myFlt = 5.99;
+    cout << myFlt << endl; 
+    double myflt1 = 2.50;
+    cout << myflt1 << endl; 
+
+    //Character
+    char myLetter = 'N';
+    cout << myLetter << endl;
+
+    //String
+    string myStr = "HEllo!";
+    cout << myStr << endl;
+
+    //Boolean 
+    bool myBoolean = true;
+    cout << myBoolean<< endl;
+
+    return 0; 
+}
