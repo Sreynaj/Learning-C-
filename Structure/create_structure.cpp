967e3c900f464f8735cb398fcdc3ac56c98c813a
Sreynaj Keb <kebsreynaj2@gmail.com>
2022-09-14 09:47:19 +0700

    sourcecode

---
+++ Structure/create_structure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    struct { //structure declarations
+    int myNum; //member (int variable)
+    string myString; //member (string variable)
+}   myStructure; // structure variable 
+
+}
